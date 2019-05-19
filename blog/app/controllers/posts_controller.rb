aaf0c4580ada721850a27db1419ea18b36d0144c
Defaf <defaf_kamfar@hotmail.com>
2019-05-19 22:19:31 +0300

    fix some errors

--- blog/app/controllers/posts_controller.rb
+++ blog/app/controllers/posts_controller.rb
@@ -23,8 +23,7 @@ class PostsController < ApplicationController
 
     def create 
         @post = Post.create (post_params)
-        # redirect_to posts_path 
-        redirect_to posts_path(@post)
+        redirect_to post_path(@post)
     end 
 
     def edit
@@ -34,7 +33,7 @@ class PostsController < ApplicationController
     def update
         @post = Post.find(params[:id])
         @post.update(post_params)
-        redirect_to posts_path(@post)
+        redirect_to post_path(@post)
     end
 
     def post_params
